f2e37bd6b5dde33f20108905dfa8f301c2ac1167
NorikaU <45717617+NorikaU@users.noreply.github.com>
2024-12-01 21:28:46 +0900

    Add files via upload

---
+++ d1p2.py
@@ -0,0 +1,35 @@
+def read_from_file(filename):
+  """Reads data from a file and returns it as a string.
+
+  Args:
+    filename: The name of the file to read from.
+
+  Returns:
+    The contents of the file as a string.
+  """
+
+  try:
+    with open(filename, 'r') as file:
+      data = file.readlines()
+    return data
+  except FileNotFoundError:
+    print(f"Error: File '{filename}' not found.")
+    return None
+
+# Example usage:
+filename = 'inputd1.txt'  # Replace with your actual file name
+file_content = read_from_file(filename)
+list1,list2 = [],[]
+
+sum = 0
+if file_content:
+    for line in file_content:
+        x,y, = line.split()
+        list1.append(int(x))
+        list2.append(int(y))
+
+    for i in list1:
+        sum += i*list2.count(i)
+        
+        
+print(sum)
