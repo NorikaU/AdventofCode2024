8260d54730b514886748bfdd50f8cd621a86b13c
NorikaU <45717617+NorikaU@users.noreply.github.com>
2024-12-03 15:03:14 +0900

    Create d3p1.py

---
+++ Day3/d3p1.py
@@ -0,0 +1,36 @@
+def read_from_file(filename):
+  try:
+    with open(filename, 'r') as file:
+      data = file.readlines()
+    return data
+  except FileNotFoundError:
+    print(f"Error: File '{filename}' not found.")
+    return None
+
+def isSafe(report):
+    report = [int(i) for i in line.split()]
+    if sorted(report) != report and sorted(report, reverse=True) != report:
+        return False
+        
+    for j in range(1,len(report)):
+        if abs(report[j]-report[j-1])>3:
+            return False
+        if report[j] == report[j-1]:
+            return False
+    return True
+  
+filename = 'Day2/input.txt'  
+file_content = read_from_file(filename)  
+
+safe_lines = 0
+if file_content:
+    for line in file_content:
+        safe = True
+        report = [int(i) for i in line.split()]
+        
+        if isSafe(report):
+            safe_lines += 1
+        else:
+            print(report)
+            
+print(safe_lines)
